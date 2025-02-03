d76d59537c3b387950d142c93ea689b8cfa8e2af
Lorenzo Nava <89015253+lorenzonava96@users.noreply.github.com>
2025-02-03 15:02:58 +0000

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .preprocessing import *
 from .offset_track import *
 from .postprocessing import *
 from .time_series_generation import *
-from. acquisition import *
+from .acquisition import *
